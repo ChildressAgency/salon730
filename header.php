059a12019ee7d1ab7befdb29b32978dba07c4a99
halfg33k <johnsonam17@gmail.com>
2019-01-23 15:15:05 -0500

    header

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width initial-scale=1.0" name="viewport">
+    <meta content="The Childress Agency" name="author">
+    <meta content="public" http-equiv="cache-control">
+    <meta content="private" http-equiv="cache-control">
+    
+    <title><?php echo get_bloginfo( 'name' ); if( get_bloginfo( 'description' ) ): echo ' | ' . get_bloginfo( 'description' ); endif; ?></title>
+
+    <?php wp_head(); ?>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
+    <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
+    <![endif]-->
+    <!--[if gte IE 9]
+    <style type='text/css'>
+    footer {
+    filter: none;
+    }
+    </style>
+    <![endif]-->
+</head>
+<body>
+    
+    <header class="header">
+        <div class="header__brand">
+            <img src="http://dev.childressagency.com/salon730/wp-content/uploads/2019/01/Logo.png" />
+        </div>
+        <div class="header__links">
+            <a class="header__call" href="tel:540-373-6040">
+                <p>CALL TODAY</p>
+                <p class="header__number">540-373-6040</p>
+            </a>
+            <div class="header__nav">
+                <button class="navbar__toggler" type="button" data-target="#main-menu">
+                    <i class="fas fa-bars"></i>
+                </button>
+                
+                <div id="main-menu">
+                    <?php 
+                    wp_nav_menu( array(
+                        'theme_location'    =>  'main_menu',
+                        'menu_class'        =>  'navbar__nav',
+                        'walker'            =>  new Custom_Nav_Walker()
+                    ) ); ?>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ js/salon730-script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    /**
+     * HEADER NAVIGATION
+     */
+    $header_nav = $( '.header__nav' ).find( '.navbar__nav' );
+    $header_nav_height = 0;
+
+    $( '.navbar__toggler' ).click(function(){
+            $header_nav_height = 0;
+            $header_nav.children().each(function(){
+                $header_nav_height += $( this ).outerHeight() + parseInt( $( this ).css('marginTop') ) + parseInt( $( this ).css('marginBottom') );
+            });
+
+        if( $header_nav.hasClass( 'navbar__open' ) ){
+            $header_nav.removeClass( 'navbar__open' );
+            $header_nav.css( 'max-height', '0' );
+        } else {
+            $header_nav.addClass( 'navbar__open' );
+            $header_nav.css( 'max-height', $header_nav_height + 'px' );
+        }
+    });
+
+    $('.featured-slider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 1,
+        arrows: false,
+        autoplaySpeed: 5000
+    });
+});
