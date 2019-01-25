9c7c64c6011e3889d392c5a1237736c81ee32248
halfg33k <johnsonam17@gmail.com>
2019-01-25 16:36:05 -0500

    added color panel dependency

--- js/block-dependencies.js
+++ js/block-dependencies.js
@@ -1,4 +1,4 @@
 const { registerBlockType } = wp.blocks;
-const { RichText, BlockControls, InspectorControls, AlignmentToolbar, MediaUpload, FontSizePicker, PlainText, InnerBlocks, URLInputButton } = wp.editor;
+const { RichText, BlockControls, InspectorControls, AlignmentToolbar, MediaUpload, FontSizePicker, PlainText, InnerBlocks, URLInputButton, PanelColorSettings } = wp.editor;
 const { Fragment } = wp.element;
 const { Button, SelectControl, ToggleControl, ServerSideRender, Panel, PanelBody } = wp.components;
